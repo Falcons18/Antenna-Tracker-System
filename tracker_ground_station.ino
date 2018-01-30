d459c53ba70abd4aedf60a607c84948e2e1156f1
Falcons18 <32963557+Falcons18@users.noreply.github.com>
2018-01-30 23:26:52 +0530

    Add files via upload

---
+++ tracker_ground_station.ino
@@ -0,0 +1,27 @@
+#include<Servo.h>
+
+Servo servo_pan, servo_tilt;
+
+void setup() 
+{
+  Serial.begin(57600);
+  servo_pan.attach(6);
+  servo_pan.write(0);
+  servo_tilt.attach(3); 
+  servo_tilt.write(0);
+  
+  Serial.setTimeout(350);
+  delay(3000); 
+}
+int p,t;
+void loop() 
+{
+  while(!Serial.available()){}
+  while(Serial.available()){
+    p = Serial.parseInt();
+    t = Serial.parseInt();
+  }
+  
+  servo_pan.write(p);
+  servo_tilt.write(t);
+}
